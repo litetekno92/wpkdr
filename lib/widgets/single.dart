fcbb3f3cbdffa4770bc73ed03e77cd9fa881be67
LiteTekno92 <lite.tekno92@gmail.com>
2019-04-17 12:08:53 +0200

    Single modified

--- lib/widgets/single.dart
+++ lib/widgets/single.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:wpkdr/models/embedpost.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+
+
 
 class SinglePost extends StatelessWidget {
 
@@ -19,14 +23,34 @@ class SinglePost extends StatelessWidget {
         decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
           child: new ListView(
           children: <Widget>[
-            new FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image: post.featuredMedia == 0
-                  ? 'images/placeholder.png'
-                  : post.featuredMediaUrl,
-            ),
-            new Text(post.content.rendered.replaceAll(new RegExp(r'<[^>]*>'), ''),style: TextStyle(color: Colors.white))
+            // new FadeInImage.memoryNetwork(
+            //   placeholder: kTransparentImage,
+            //   image: post.featuredMedia == 0
+            //       ? 'images/placeholder.png'
+            //       : post.featuredMediaUrl,
+            // ),
+            FadeInImage.assetNetwork(
+          placeholder: 'images/placeholder.png',
+          image: post.featuredMediaUrl ?? 'https://picsum.photos/250?image=9',
+        ),
+            new Text(post.content.rendered.replaceAll(new RegExp(r'<[^>]*>'), ''),style: TextStyle(color: Colors.white)),
+            Divider(color: Colors.white24),
+            Row(
+              children: [
+                Text(
+              DateFormat('yyyy-MM-dd – kk:mm').format(post.date).toString(), style: TextStyle(color: Colors.white)),
+                IconButton(
+                  icon: new Icon(FontAwesomeIcons.shareAlt), 
+                  onPressed: () { print("Pressed");},
+                ),
+                IconButton(
+                  icon: new Icon(FontAwesomeIcons.save), 
+                  onPressed: () { print("Pressed");},
+                ),
+              ]
+            )
           ],
+
         ),
       ),
     )
